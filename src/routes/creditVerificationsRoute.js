e3b7458bc3c4b48d10b4d359b319fd41906c1251
octavioserrago <octavioserrago.dev@gmail.com>
2024-08-01 16:45:40 -0300

    Fix: Fixed abd error with the route updateRealAmount

--- src/routes/creditVerificationsRoute.js
+++ src/routes/creditVerificationsRoute.js
@@ -9,7 +9,7 @@ router.get('/:ID', creditVerificationsController.show);
 router.put('/:ID', creditVerificationsController.update);
 router.delete('/:ID', creditVerificationsController.delete);
 router.get('/findByBranchId/:branch_id', creditVerificationsController.findByBranchId);
-router.put('/updateRealAmount', creditVerificationsController.updateRealAmount);
+router.put('/updateRealAmount/:creditVerification_id', creditVerificationsController.updateRealAmount);
 
 module.exports = router;
 
